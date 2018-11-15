501c87bb5bd8eb3cf0b450b3e4048f2f1b03b8dd
george <george_li@gwu.edu>
2018-11-15 23:26:47 +0000

    loss visualization remove tqdm

--- Prototype/V1-George.py
+++ Prototype/V1-George.py
@@ -2,7 +2,6 @@ import tensorflow as tf
 print(tf.__version__)
 
 import numpy as np
-from tqdm import tqdm
 
 label2expression = {1: "Surprise",2: "Fear",3: "Disgust",4: "Happiness",
                     5: "Sadness", 6: "Anger",7: "Neutral"}
@@ -30,17 +29,17 @@ def load_to_numpy(img_label_pair):
     labels = np.zeros((limit, 7))
     imgs = np.empty((limit, length, width, 3))
     print("Loading Data:")
-    with tqdm(total=limit) as pbar:
-        i = 0
-        for image_name in img_label_pair:
-            img = Image.open(folder + image_name).convert('RGB')
-            img = np.array(img).reshape((100,100,3))
-            # imgs = np.append(imgs, img, axis=0)
-            imgs[i] = img # faster approach! learning algorithm is useful
-            #labels[i] = img_label_pair[image_name]
-            labels[i, img_label_pair[image_name]-1] = 1
-            i+=1
-            pbar.update(1)
+
+    i = 0
+    for image_name in img_label_pair:
+        img = Image.open(folder + image_name).convert('RGB')
+        img = np.array(img).reshape((100,100,3))
+        # imgs = np.append(imgs, img, axis=0)
+        imgs[i] = img # faster approach! learning algorithm is useful
+        #labels[i] = img_label_pair[image_name]
+        labels[i, img_label_pair[image_name]-1] = 1
+        i+=1
+
     one_hot_label = np.zeros(())
     return (imgs, labels)
 
@@ -62,5 +61,19 @@ model.compile(optimizer=tf.train.AdadeltaOptimizer(0.001),
               loss='categorical_crossentropy',
              metrics=[tf.keras.metrics.categorical_accuracy])
 
-model.fit(train_img, train_label, epochs=1,batch_size=32)
-print(model.evaluate(test_img, test_label,batch_size=32))
+num_epoch = 200
+
+history = model.fit(train_img, train_label, epochs=num_epoch, batch_size=32)
+
+import matplotlib.pyplot as plt
+plt.plot(np.linspace(1, num_epoch, num_epoch), np.array(history.history["categorical_accuracy"]), label='Accuracy')
+plt.legend()
+plt.show()
+plt.plot(np.linspace(1, num_epoch, num_epoch), np.array(history.history["loss"]), label='loss')
+plt.legend()
+plt.show()
+
+
+loss, accuracy = model.evaluate(test_img, test_label,batch_size=32)
+print("test loss:", loss)
+print("test accuracy:", accuracy)
