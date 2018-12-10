b17e50fbc9eaaab8bc23f66880c094afb96b003e
zhu_liwei <zhu_liwei29@gwu.edu>
2018-12-10 17:31:15 -0500

    folders upload

---
+++ Liwei-Zhu-individual-project/code/V2_main.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3.5
+
+"""
+Prototype V2
+Main Program
+By: Xiaochi (George) Li & Adjusted by Liwei Zhu
+Nov.2018
+"""
+
+
+import V2_helper
+import V2_models
+import Xiaochi_models
+
+
+num_epoch = 100
+batch_size = 64
+input_shape = (100, 100)
+
+model = Xiaochi_models.light_vgg2()
+
+
+num_epoch = 100
+batch_size = 32
+model = V2_models.sgd_model()
+log_name = input("What's the name of this run?:")
+
+# create image label pair as a dictionary
+label_file = "../RAFDB/list_patition_label.txt"
+folder = "../RAFDB/aligned/"
+train_img_label_pair, test_img_label_pair = V2_helper.label_loader(label_file)
+
+# Use pickle to speed up reading data
+import pickle
+print("Loading Data")
+try:
+    train_img, train_label, test_img, test_label = pickle.load(open("processed_data_"+str(input_shape)+".pickle", 'rb'))
+    print("Shape", train_img.shape)
+except:
+    train_img, train_label = V2_helper.load_to_numpy(train_img_label_pair, folder, input_shape)
+    test_img, test_label = V2_helper.load_to_numpy(test_img_label_pair, folder, input_shape)
+    print("Shape", train_img.shape)
+    print("First time reading data, saving to pickle to speed up next time")
+    try:
+        pickle.dump((train_img, train_label, test_img, test_label), open("processed_data_"+str(input_shape)+".pickle", 'wb'))
+    except:
+        print("can't save, file too big")
+
+# Keras model and Tensor Board
+from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint
+import time
+tb = TensorBoard(log_dir="logs/" + log_name +" "+ time.ctime())
+chk = ModelCheckpoint("./trained_model/"+log_name+"_chk.h5", monitor='val_categorical_accuracy', verbose=0,
+                      save_best_only=True, save_weights_only=True, mode='max', period=1)
+history = model.fit(train_img, train_label, epochs=num_epoch, batch_size=batch_size,
+                    validation_data=(test_img, test_label), callbacks=[tb, chk])
+model.summary()
+
+V2_helper.plot(history, log_name, num_epoch)
+import os
+if not os.path.exists("./trained_model"):
+    print("First run, make dir")
+    os.makedirs("./trained_model")
+model.save("./trained_model/"+log_name+".h5")
+loss, accuracy = model.evaluate(test_img, test_label,batch_size=32)
+print("test loss:", loss)
+print("test accuracy:", accuracy)
---
+++ Liwei-Zhu-individual-project/code/V2_models.py
@@ -0,0 +1,159 @@
+"""
+Prototype V2
+Different Keras Models
+By: Liwei Zhu
+Nov.2018
+"""
+
+import tensorflow as tf
+from tensorflow.keras import layers
+
+# Accuracy around 65% (epoch=200)
+def first_model():
+    model = tf.keras.Sequential([
+        layers.Conv2D(64, kernel_size=3, activation='relu', input_shape=(100,100,3)),
+        layers.Conv2D(32, kernel_size=3, activation='relu'),
+        layers.Flatten(),
+        layers.Dense(7, activation='softmax')
+    ])
+
+    model.compile(optimizer=tf.train.AdadeltaOptimizer(0.001),
+                  loss='categorical_crossentropy',
+                 metrics=[tf.keras.metrics.categorical_accuracy])
+
+    return model
+
+
+# Adding the maxpooling layer
+# Accuracy around 66% (epoch=200)
+def second_model():
+    model = tf.keras.Sequential([
+        layers.Conv2D(64, kernel_size=3, activation='relu', input_shape=(100,100,3)),
+        layers.Conv2D(32, kernel_size=3, activation='relu'),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Flatten(),
+        layers.Dense(7, activation='softmax')
+    ])
+
+    model.compile(optimizer=tf.keras.optimizers.Adadelta(0.001),
+                  loss='categorical_crossentropy',
+                 metrics=[tf.keras.metrics.categorical_accuracy])
+
+    return model
+
+# Modified the filter number
+# Accuracy around 66% (epoch=100)
+def third_model():
+    model = tf.keras.Sequential([
+        layers.Conv2D(128, kernel_size=3, activation='relu', input_shape=(100,100,3)),
+        layers.Conv2D(128, kernel_size=3, activation='relu'),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Flatten(),
+        layers.Dense(7, activation='softmax')
+    ])
+
+    model.compile(optimizer=tf.keras.optimizers.Adadelta(0.001),
+                  loss='categorical_crossentropy',
+                 metrics=[tf.keras.metrics.categorical_accuracy])
+
+    return model
+
+# Adding the dense and dropout layers
+# Accuracy around 66.62% (epoch=100)
+def fourth_model():
+    model = tf.keras.Sequential([
+        layers.Conv2D(128, kernel_size=3, activation='relu', input_shape=(100,100,3)),
+        layers.Conv2D(128, kernel_size=3, activation='relu'),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Dropout(0.1),
+        layers.Flatten(),
+        layers.Dense(32, activation='relu'),
+        layers.Dropout(0.1),
+        layers.Dense(7, activation='softmax')
+    ])
+
+    model.compile(optimizer=tf.keras.optimizers.Adadelta(0.001),
+                  loss='categorical_crossentropy',
+                 metrics=[tf.keras.metrics.categorical_accuracy])
+
+    return model
+
+# Modified with Adam optimizer
+# Accuracy around 96%/68.84% (epoch=100)
+def fifth_model():
+    model = tf.keras.Sequential([
+        layers.Conv2D(128, kernel_size=3, activation='relu', input_shape=(100,100,3)),
+        layers.Conv2D(128, kernel_size=3, activation='relu'),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Dropout(0.1),
+        layers.Flatten(),
+        layers.Dense(32, activation='relu'),
+        layers.Dropout(0.1),
+        layers.Dense(7, activation='softmax')
+    ])
+
+    model.compile(optimizer=tf.keras.optimizers.Adam(lr=0.001),
+                  loss='categorical_crossentropy',
+                 metrics=[tf.keras.metrics.categorical_accuracy])
+
+    return model
+
+# Modified with Adadelta optimizer
+# Accuracy around 99%/74% (epoch=100)
+def Adadelta_model():
+    model = tf.keras.Sequential([
+        layers.Conv2D(128, kernel_size=3, activation='relu', input_shape=(100,100,3)),
+        layers.Conv2D(128, kernel_size=3, activation='relu'),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Dropout(0.1),
+        layers.Flatten(),
+        layers.Dense(32, activation='relu'),
+        layers.Dropout(0.1),
+        layers.Dense(7, activation='softmax')
+    ])
+
+    model.compile(optimizer=tf.keras.optimizers.Adadelta(lr=1.0),
+                  loss='categorical_crossentropy',
+                 metrics=[tf.keras.metrics.categorical_accuracy])
+
+    return model
+
+# Modified with RMSprop optimizer
+# Accuracy around 95%/67% (epoch=100)
+def rmsprop_model():
+    model = tf.keras.Sequential([
+        layers.Conv2D(128, kernel_size=3, activation='relu', input_shape=(100,100,3)),
+        layers.Conv2D(128, kernel_size=3, activation='relu'),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Dropout(0.1),
+        layers.Flatten(),
+        layers.Dense(32, activation='relu'),
+        layers.Dropout(0.1),
+        layers.Dense(7, activation='softmax')
+    ])
+
+    model.compile(optimizer=tf.keras.optimizers.RMSprop(lr=0.001),
+                  loss='categorical_crossentropy',
+                 metrics=[tf.keras.metrics.categorical_accuracy])
+
+    return model
+
+# Modified with SGD optimizer
+# Accuracy around 99%/73% (epoch=100)
+def sgd_model():
+    model = tf.keras.Sequential([
+        layers.Conv2D(128, kernel_size=3, activation='relu', input_shape=(100,100,3)),
+        layers.Conv2D(128, kernel_size=3, activation='relu'),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Dropout(0.1),
+        layers.Flatten(),
+        layers.Dense(32, activation='relu'),
+        layers.Dropout(0.1),
+        layers.Dense(7, activation='softmax')
+    ])
+
+    model.compile(optimizer=tf.keras.optimizers.SGD(lr=0.01),
+                  loss='categorical_crossentropy',
+                 metrics=[tf.keras.metrics.categorical_accuracy])
+
+    return model
